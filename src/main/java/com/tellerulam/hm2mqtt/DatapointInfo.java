c7a1b46710d8085b04133aaa324483823a0f6a93
owagner <owagner@tellerulam.com>
2015-03-14 22:45:42 +0100

    - add missing class

---
+++ src/main/java/com/tellerulam/hm2mqtt/DatapointInfo.java
@@ -0,0 +1,40 @@
+package com.tellerulam.hm2mqtt;
+
+import java.io.*;
+import java.util.*;
+
+public class DatapointInfo implements Serializable
+{
+	private static final long serialVersionUID = 2L;
+
+	public final String name;
+	public final HMValueTypes type;
+	public final String unit;
+	public final String enumValues[];
+
+	public boolean isAction()
+	{
+		return type==HMValueTypes.ACTION;
+	}
+
+	private DatapointInfo(String name, HMValueTypes type, String unit, String[] enumValues)
+	{
+		this.name = name;
+		this.type = type;
+		this.unit = unit;
+		this.enumValues = enumValues;
+	}
+
+	public static DatapointInfo constructFromParamsetDescription(Map<String, Object> paramset)
+	{
+		@SuppressWarnings("unchecked")
+		Collection<String> enumValues=(Collection<String>)paramset.get("VALUE_LIST");
+
+		return new DatapointInfo(
+			(String)paramset.get("ID"),
+			HMValueTypes.valueOf((String)paramset.get("TYPE")),
+			(String)paramset.get("UNIT"),
+			enumValues!=null?enumValues.toArray(new String[enumValues.size()]):null
+			);
+	}
+}
